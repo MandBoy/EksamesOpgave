a32826a5c1639973fdde0399cecfa34dbb6fcb56
Leopold Berggreen Nguessan <leopold1@live.dk>
2019-05-20 22:09:22 +0200

    homeController osv..

---
+++ Code/EksamesProject/src/service/DemoEksamensprojekt.java
@@ -0,0 +1,16 @@
+package EksamesProject.src.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEksamensprojekt {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DemoEksamensprojekt.class, args);
+
+    }
+}
