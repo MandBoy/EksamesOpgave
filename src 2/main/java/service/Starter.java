b77d6f65f0a5dcda90e74b092620a067061a5efa
Leopold Berggreen Nguessan <leopold1@live.dk>
2019-05-21 12:55:10 +0200

    Ændring

--- src 2/main/java/service/Starter.java
+++
@@ -1,8 +0,0 @@
-public class Starter {
-    public static void main(String[]args){
-        system();
-    }
-    public static void system(){
-        BorrowSystem borrowSystem = new BorrowSystem();
-    }
-}
