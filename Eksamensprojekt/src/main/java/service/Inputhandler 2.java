170f2f01d82c33611bb888b8d808de30b106cc4c
Leopold Berggreen Nguessan <leopold1@live.dk>
2019-05-23 13:34:08 +0200

    F

---
+++ Eksamensprojekt/src/main/java/service/Inputhandler 2.java
@@ -0,0 +1,34 @@
+/*
+ *Made by Andreas Lind.
+ * Used to take input saving us from creating a scanner in every class.
+ */
+import java.util.*;
+
+public class Inputhandler{
+    public static Scanner scanner = new Scanner(java.lang.System.in);
+    public static String strChoice(){
+        String str = scanner.next();
+        return str;
+    }
+    public static int intChoice() {
+        while(!scanner.hasNextInt()) {
+            scanner.next();
+        }
+        return scanner.nextInt();
+    }
+    public static double douChoice() {
+        while(!scanner.hasNextDouble()) {
+            scanner.next();
+        }
+        return scanner.nextDouble();
+    }
+    //Needs physical scanner.
+    //We needed to cut this part since tech lounge didn't have an extra scanner we could borrow.
+    public static int itemScan(){
+        while(!scanner.hasNextInt()){
+            scanner.next();
+        }
+        return scanner.nextInt();
+    }
+}
+
