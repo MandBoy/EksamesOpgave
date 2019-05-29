6d807b337bf1417c96c431410b2e5bfa42d422e2
Leopold Berggreen Nguessan <leopold1@live.dk>
2019-05-29 12:58:14 +0200

    Cleanup af repository

---
+++ src/main/java/repository/ItemRepo.java
@@ -0,0 +1,49 @@
+/*
+Lavet af Leopold.
+ */
+
+package repository;
+
+import model.Item;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+@Repository
+public class ItemRepo {
+    @Autowired
+    JdbcTemplate template;
+
+    public List<Item> fetchAllItems(){
+        String sql =" SELECT * FROM Item";
+        RowMapper<Item> rowMapper = new BeanPropertyRowMapper<>(Item.class);
+        return template.query(sql, rowMapper);
+    }
+
+    public void readById(int itemId){
+        String sql = "SELECT * FROM bruger WHERE brugerId=?";
+        RowMapper<Item> rowMapper = new BeanPropertyRowMapper<>(Item.class);
+        template.queryForObject(sql, rowMapper, itemId);
+    }
+
+    public void createItem(Item item){
+        String sql = "insert into Bruger (navn, pris, opløsning, vægt, udgivelse, model) values(?, ?, ?, ?, ?, ? )";
+        RowMapper<Item> rowMapper = new BeanPropertyRowMapper<>(Item.class);
+        template.update(sql, item.getNavn(), item.getPris(),item.getOpløsning(), item.getVægt(), item.getUdgivelse(), item.getModel() );
+    }
+
+    public void updateItem(Item item){
+        String sql ="Update Item SET navn=?, pris=?, opløsning=?, vægt=?, udgivelse=?, model=? WHERE itemId=? ";
+        template.update(sql, item.getNavn(), item.getPris(),item.getOpløsning(), item.getVægt(), item.getUdgivelse(), item.getModel());
+    }
+
+    public void deleteById(int itemId){
+        String sql = " Delete from Item WHERE id=?";
+        template.update(sql, itemId);
+    }
+}
+
