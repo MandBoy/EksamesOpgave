1e7b43b0fff0e1f8ba194a697fe6a314c0fb7a40
Andreas Lind <32731570+MandBoy@users.noreply.github.com>
2019-07-25 16:21:51 +0200

    F

--- src/main/java/com/example/EksamesOpgave/demo/DemoApplication.java
+++ src/main/java/com/example/EksamesOpgave/demo/DemoApplication.java
@@ -2,12 +2,11 @@
 Lavet a Leopold.
  */
 package com.example.EksamesOpgave.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication{
     public static void main(String[] args){
         SpringApplication.run(DemoApplication.class, args);
     }
